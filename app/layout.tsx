c609f9361a40949861cf75ba98e6bd17179373b3
KhaledAlabssi <khaled.absi5@gmail.com>
2024-06-01 14:59:16 +0200

    Challenge 1: daisyui & app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,12 +6,12 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "nextJS Tutorial",
-  description: "Learning nextJS is fun", 
+  description: "Learning nextJS is fun",
   keywords: ['Next.js', 'React', 'TypeScript', 'Coding', 'Node.js'],
   authors: [
     { name: 'Khaled Alabssi', url: 'https://github.com/KhaledAlabssi' }
   ]
-  
+
 };
 
 export default function RootLayout({
@@ -21,14 +21,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      {/* <p>Paragraph before body element!</p> */}
       <body className={inter.className}>
-        {/* before childern element */}
-        <nav className="bg-gray-300">Welcome toNextJS APP "nav"</nav>
+        <div className="navbar bg-primary text-primary-content">
+          <button className="btn btn-ghost text-xl">daisyUI</button>
+        </div>
         {children}
-        <footer className="bg-gray-300">Welcome to Nextjs App "footer"</footer>
-        </body>
-      {/* <p>Paragraph after body element!</p> */}
+        <div className="btm-nav">
+          <button>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
+          </button>
+          <button className="active">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+          </button>
+          <button>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" /></svg>
+          </button>
+        </div>
+      </body>
     </html>
   );
 }
